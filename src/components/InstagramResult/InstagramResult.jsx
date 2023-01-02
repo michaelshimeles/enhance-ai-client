a44dc63379ba15117ec6121f5285f9e8f809f02b
Michael Shimeles <michaelwasihun96@gmail.com>
2023-01-02 16:45:31 -0500

    Can copy ig caption to clipboard

--- src/components/InstagramResult/InstagramResult.jsx
+++ src/components/InstagramResult/InstagramResult.jsx
@@ -1,9 +1,17 @@
+import "./InstagramResult.scss";
 export const InstagramResult = ({ className, newResult }) => {
   return (
     <div className={className}>
       <ol>
         {newResult.slice(1).map((line, index) => (
-          <li key={index}>{line}</li>
+          <div className="ig-result" key={index}>
+            {line}
+            <button onClick={() => navigator.clipboard.writeText(`${line}`)}
+            className="ig-result__button"
+            >
+              Copy
+            </button>
+          </div>
         ))}
       </ol>
     </div>
