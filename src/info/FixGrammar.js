eccbaac8905dccc6567508d063060410a0623625
Michael Shimeles <michaelwasihun96@gmail.com>
2023-01-05 19:21:42 -0500

    Fixed image links

--- src/info/FixGrammar.js
+++ src/info/FixGrammar.js
@@ -1,10 +1,9 @@
-import copy from '../assets/images/copy.jpg';
-
 export const fixGrammar = {
   title: 'Grammar Fixer',
   description: `Our advanced grammar fixing tool utilizes OpenAI's technology to quickly and accurately identify and correct errors, helping you elevate your writing.`,
-  cta: "Fix My Grammar",
-  image: copy,
+  cta: 'Fix My Grammar',
+  image:
+    'https://images.unsplash.com/photo-1582137490248-c53207ec5602?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80',
   launched: true,
-  link: "/grammar"
+  link: '/grammar',
 };
