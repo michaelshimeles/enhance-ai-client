7b051ace0c9b8ccd59dd93bdb4f970e70659cdbc
Michael Shimeles <michaelwasihun96@gmail.com>
2023-01-06 00:02:03 -0500

    Added typwriter effect in herosection

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,19 +1,17 @@
-import { Flex, Heading } from '@chakra-ui/react';
-// import Typewriter from 'typewriter-effect';
+import { Flex } from '@chakra-ui/react';
+import Typewriter from 'typewriter-effect';
+import './HeroSection.scss';
 
 export const HeroSection = () => {
   return (
     <Flex justify="center" align="center" pt="2rem">
-      {/* <Typewriter
+      <Typewriter
         options={{
           strings: ['EnhanceAI', 'Enhance Captions', 'Enhance Grammar'],
           autoStart: true,
           loop: true,
         }}
-      /> */}
-      <Heading as="h1" size="3xl">
-        Enhance AI
-      </Heading>
+      />
     </Flex>
   );
 };
