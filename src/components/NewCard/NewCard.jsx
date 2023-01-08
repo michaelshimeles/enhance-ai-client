b9e03b2e99677e7d991154041d52b66f8e4f5f46
Michael Shimeles <michaelwasihun96@gmail.com>
2023-01-07 23:02:00 -0500

    Added a card layout to homepage

---
+++ src/components/NewCard/NewCard.jsx
@@ -0,0 +1,100 @@
+import {
+  Flex,
+  Image,
+  Box,
+  Link,
+  chakra,
+  Badge,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { Link as ReachLink } from 'react-router-dom';
+
+export const NewCard = ({ image, description, title, link, cta, launched }) => {
+  const bgColor = useColorModeValue('white', 'black');
+
+  return (
+    <Link as={ReachLink} to={link} _hover={{ textDecoration: 'none' }}>
+      <Flex
+        alignItems="center"
+        justifyContent="center"
+        maxW="sm"
+        bgColor={bgColor}
+      >
+        <Box
+          mx="auto"
+          rounded="lg"
+          shadow="md"
+          // bg="white"
+          _dark={{
+            bg: 'gray.800',
+          }}
+          maxW="2xl"
+        >
+          <Image
+            roundedTop="lg"
+            w="full"
+            h={64}
+            fit="cover"
+            src={image}
+            alt="Article"
+          />
+
+          <Box p={6}>
+            <Box>
+              <chakra.span
+                fontSize="xs"
+                textTransform="uppercase"
+                color="brand.600"
+                _dark={{
+                  color: 'brand.400',
+                }}
+              >
+                {cta}
+              </chakra.span>
+              <Link
+                as={ReachLink}
+                display="block"
+                color="gray.800"
+                _dark={{
+                  color: 'white',
+                }}
+                fontWeight="bold"
+                fontSize="2xl"
+                mt={2}
+                _hover={{
+                  color: 'gray.600',
+                  textDecor: 'underline',
+                  textDecoration: 'none',
+                }}
+              >
+                {title}
+              </Link>
+              <chakra.p
+                mt={2}
+                fontSize="sm"
+                color="gray.600"
+                _dark={{
+                  color: 'gray.400',
+                }}
+              >
+                {description}
+              </chakra.p>
+            </Box>
+
+            <Box mt={4}>
+              <Flex alignItems="center">
+                <Flex alignItems="center">
+                  {!launched ? (
+                    <Badge colorScheme="purple">Coming Soon</Badge>
+                  ) : (
+                    <Badge colorScheme="green">It's Live</Badge>
+                  )}
+                </Flex>
+              </Flex>
+            </Box>
+          </Box>
+        </Box>
+      </Flex>
+    </Link>
+  );
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,39 +6,78 @@ import { igCaption } from '../../info/InstagramCaptions';
 import { fixGrammar } from '../../info/FixGrammar';
 import { resumeBuilder } from '../../info/ResumeBuilder';
 import { Layout } from '../../components/Layout/Layout';
-
+import { NewCard } from '../../components/NewCard/NewCard';
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 export const Home = () => {
   return (
     <Layout>
       <Flex direction="column" justify="center" align="center" w="full">
         <NavBar />
         <HeroSection />
-        <VStack spacing="1rem" pt="3rem" maxW={'90%'}>
-          <CardAI
-            title={igCaption.title}
-            description={igCaption.description}
-            cta={igCaption.cta}
-            image={igCaption.image}
-            launched={igCaption.launched}
-            link={igCaption.link}
-          />
-          <CardAI
-            title={fixGrammar.title}
-            description={fixGrammar.description}
-            cta={fixGrammar.cta}
-            image={fixGrammar.image}
-            launched={fixGrammar.launched}
-            link={fixGrammar.link}
-          />
-          <CardAI
-            title={resumeBuilder.title}
-            description={resumeBuilder.description}
-            cta={resumeBuilder.cta}
-            image={resumeBuilder.image}
-            launched={resumeBuilder.launched}
-            link={resumeBuilder.link}
-          />
-        </VStack>
+        <Tabs variant="soft-rounded" colorScheme="green" w="80%">
+          <TabList>
+            <Tab>Stacked</Tab>
+            <Tab>Card</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <VStack spacing="1rem" pt="3rem" maxW='100%'>
+                <CardAI
+                  title={igCaption.title}
+                  description={igCaption.description}
+                  cta={igCaption.cta}
+                  image={igCaption.image}
+                  launched={igCaption.launched}
+                  link={igCaption.link}
+                />
+                <CardAI
+                  title={fixGrammar.title}
+                  description={fixGrammar.description}
+                  cta={fixGrammar.cta}
+                  image={fixGrammar.image}
+                  launched={fixGrammar.launched}
+                  link={fixGrammar.link}
+                />
+                <CardAI
+                  title={resumeBuilder.title}
+                  description={resumeBuilder.description}
+                  cta={resumeBuilder.cta}
+                  image={resumeBuilder.image}
+                  launched={resumeBuilder.launched}
+                  link={resumeBuilder.link}
+                />
+              </VStack>
+            </TabPanel>
+            <TabPanel>
+              <Flex justify="center" align="flex-start" gap="1rem" pt="3rem" flexWrap="wrap">
+                <NewCard
+                  title={igCaption.title}
+                  description={igCaption.description}
+                  cta={igCaption.cta}
+                  image={igCaption.image}
+                  launched={igCaption.launched}
+                  link={igCaption.link}
+                />
+                <NewCard
+                  title={fixGrammar.title}
+                  description={fixGrammar.description}
+                  cta={fixGrammar.cta}
+                  image={fixGrammar.image}
+                  launched={fixGrammar.launched}
+                  link={fixGrammar.link}
+                />
+                <NewCard
+                  title={resumeBuilder.title}
+                  description={resumeBuilder.description}
+                  cta={resumeBuilder.cta}
+                  image={resumeBuilder.image}
+                  launched={resumeBuilder.launched}
+                  link={resumeBuilder.link}
+                />
+              </Flex>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </Flex>
     </Layout>
   );
