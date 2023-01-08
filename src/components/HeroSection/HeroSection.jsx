653ffff22f7536a0b19611e8c90f50cf611f3f38
Michael Shimeles <michaelwasihun96@gmail.com>
2023-01-07 23:09:12 -0500

    Fixed font issue on mobile

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,17 +1,22 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Hide, Show, Heading } from '@chakra-ui/react';
 import Typewriter from 'typewriter-effect';
 import './HeroSection.scss';
 
 export const HeroSection = () => {
   return (
     <Flex justify="center" align="center" pt="2rem">
-      <Typewriter
-        options={{
-          strings: ['EnhanceAI', 'Enhance Captions', 'Enhance Grammar'],
-          autoStart: true,
-          loop: true,
-        }}
-      />
+      <Hide below="lg">
+        <Typewriter
+          options={{
+            strings: ['EnhanceAI', 'Enhance Captions', 'Enhance Grammar'],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </Hide>
+      <Show below="lg">
+        <Heading pb="2rem" size="3xl">EnhanceAI</Heading>
+      </Show>
     </Flex>
   );
 };
