4a5bde8ed243af084651026c6a9ebbe76216e077
Michael Shimeles <michaelwasihun96@gmail.com>
2023-01-19 16:49:48 -0500

    Started profile dashboard

--- src/pages/ProfileDashboard/ProfileDashboard.jsx
+++ src/pages/ProfileDashboard/ProfileDashboard.jsx
@@ -1,5 +1,21 @@
-import { Button, Heading, Progress, Text, VStack } from '@chakra-ui/react';
-import { signOut } from 'firebase/auth';
+import {
+  Box,
+  Button,
+  Flex,
+  FormLabel,
+  Heading,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Progress,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import { Layout } from '../../components/Layout/Layout';
@@ -8,12 +24,20 @@ import { NavBar } from '../../components/NavBar/NavBar';
 import { onAuthStateChanged } from 'firebase/auth';
 import { doc, getDoc } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { auth, db } from '../../Firebase';
 
 export const ProfileDashboard = () => {
   const [profileInfo, setProfileInfo] = useState({});
   const [user, loading] = useAuthState(auth);
   const navigate = useNavigate();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
+  };
 
   useEffect(() => {
     const getUserInfo = async userId => {
@@ -44,25 +68,62 @@ export const ProfileDashboard = () => {
   return !user ? null : (
     <Layout>
       <NavBar />
-      <VStack>
-        <Heading>Welcome {profileInfo?.firstName}</Heading>
-        <Text>Email: {user?.email}</Text>
-        <Button
-          onClick={() => {
-            signOut(auth)
-              .then(() => {
-                // Sign-out successful.
-                console.log('Successful Logout');
-              })
-              .catch(error => {
-                // An error happened.
-                console.log(error);
-              });
-          }}
+      <Flex justify="center" align="center" w="100%">
+        <Flex
+          direction="column"
+          justify="flex-start"
+          w="80%"
+          pt="2rem"
+          gap="0.5rem"
         >
-          Sign Out
-        </Button>
-      </VStack>
+          <Heading>
+            Welcome {profileInfo?.firstName} {profileInfo?.lastName}
+          </Heading>
+
+          <Text>Email: {user?.email}</Text>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Flex direction="column" pt="1rem" gap="0.5rem">
+              <Box>
+                <FormLabel>First Name</FormLabel>
+
+                <Input
+                  type="text"
+                  {...register('firstName', { required: true })}
+                  w={['15rem', '15rem', '30rem']}
+                />
+              </Box>
+              <Box>
+                <FormLabel>Last Name</FormLabel>
+
+                <Input
+                  type="text"
+                  {...register('lastName', { required: true })}
+                  w={['15rem', '15rem', '30rem']}
+                />
+              </Box>
+              <Button onClick={onOpen} w="20%">
+                Save Changes
+              </Button>
+              <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>Modal Title</ModalHeader>
+                  <ModalCloseButton />
+                  <ModalBody>
+                    Are you sure you want to make these changes?
+                  </ModalBody>
+
+                  <ModalFooter>
+                    <Button type="submit" variant="ghost" onClick={onClose}>
+                      Save Changes
+                    </Button>
+                  </ModalFooter>
+                </ModalContent>
+              </Modal>
+            </Flex>
+          </form>
+        </Flex>
+      </Flex>
     </Layout>
   );
 };
