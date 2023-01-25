b417441da042222583f97c214dd60168491f4f43
Michael Shimeles <michaelwasihun96@gmail.com>
2023-01-24 20:44:18 -0500

    Fixed netlify error

--- src/pages/Action/Action.jsx
+++ src/pages/Action/Action.jsx
@@ -19,7 +19,7 @@ import {
 } from 'firebase/auth';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import { Layout } from '../../components/Layout/Layout';
 import { auth } from '../../Firebase';
 import { useQuery } from '../../utils/useQuery';
@@ -32,7 +32,7 @@ export const Action = () => {
   const oobCode = query.get('oobCode');
   const continueUrl = query.get('continueUrl');
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const toast = useToast();
 
   const {
