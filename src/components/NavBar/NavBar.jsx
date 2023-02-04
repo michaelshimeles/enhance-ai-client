fc8ec8f370639ace7841e3e503d7f54fa394913a
Michael Shimeles <michaelwasihun96@gmail.com>
2023-02-04 10:55:49 -0500

    Add buy me coffee button

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,14 @@
 import {
   Avatar,
-  Badge,
   Button,
   Flex,
   Image,
   Link,
-  Show, Text
+  Show,
+  Text
 } from '@chakra-ui/react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { BiCoffeeTogo } from 'react-icons/bi';
 import logo from '../../assets/logo/logo.png';
 import { ColorModeSwitcher } from '../../ColorModeSwitcher';
 import { auth } from '../../Firebase';
@@ -32,16 +33,22 @@ export const NavBar = () => {
           {user && (
             <Flex justify="center" align="center" gap="1rem">
               <Show above="sm">
-                <Badge colorScheme="green">Free Account</Badge>
+                <Link href="/feedback" _hover={{ textDecoration: 'none' }}>
+                  <Button variant="ghost">Feedback</Button>
+                </Link>
+                <Link
+                  href="https://www.buymeacoffee.com/mikeshimeles"
+                  _hover={{ textDecoration: 'none' }}
+                  target="_blank"
+                >
+                  <Button leftIcon={<BiCoffeeTogo />} variant="outline">
+                    Buy Me Coffee
+                  </Button>
+                </Link>
               </Show>
               <Link href="/dashboard" _hover={{ textDecoration: 'none' }}>
                 <Avatar w="2rem" h="2rem" />
               </Link>
-              <Show above="sm">
-                <Link href="/feedback" _hover={{ textDecoration: 'none' }}>
-                  <Button variant="ghost">Feedback</Button>
-                </Link>
-              </Show>
             </Flex>
           )}
           <ColorModeSwitcher />
